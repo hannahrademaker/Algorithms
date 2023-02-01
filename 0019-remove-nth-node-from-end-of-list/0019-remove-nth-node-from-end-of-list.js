bde4e1b256cf022f5b9b33acd473b5d7467583a4
Hannah Rademaker <100808897+hannahrademaker@users.noreply.github.com>
2023-02-01 10:59:19 -0500

    Time: 72 ms (54.44%), Space: 42.6 MB (52.66%) - LeetHub

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    let newLinkedList = new ListNode(0);
+    newLinkedList.next = head;
+
+    let linkedListTraverser = head;
+    let size = 0;
+
+    while (linkedListTraverser != null) {
+        linkedListTraverser = linkedListTraverser.next;
+        size++;
+    }
+    linkedListTraverser = newLinkedList;
+
+    for (let i = 0; i < size - n; i++) {
+        linkedListTraverser = linkedListTraverser.next;
+    }
+    linkedListTraverser.next = linkedListTraverser.next.next;
+    return newLinkedList.next;
+};
