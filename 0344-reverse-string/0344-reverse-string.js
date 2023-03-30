6a0e58551a7969c3e4270e700c566eb1810e1244
Hannah Rademaker <100808897+hannahrademaker@users.noreply.github.com>
2023-03-30 13:47:33 -0400

    Time: 94 ms (57.69%), Space: 49.9 MB (14.02%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -2,13 +2,16 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
-var reverseString = function(str) {
-   let start = 0; 
-    let end = str.length-1; 
-    while(start < end){
-        [str[start], str[end]] = [str[end], str[start]]; 
-        start++
-        end--
+var reverseString = function(s) {
+   let i=0
+   let j = s.length-1;
+    while(j>i){
+        let temp1=s[i];
+        let temp2=s[j];
+        s[j]=temp1;
+        s[i]=temp2;
+        i++;
+        j--;
     }
-    return str
+    return s;
 };
